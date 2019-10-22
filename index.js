23aa073d9d5058042243af6e9761fe0480ca185e
Alexandru Perianu <perianualexandru91@gmail.com>
2019-10-22 09:44:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.error ("HALP")
 console.log ("I hope it's good")
 console.warn ("Final test")
+//declare the variable
+var word
+
+//assing a value to the variable
+word = 'bird'
+console.log(word) 'bird'
+
+//assign another value to the variable
+word = 'dog'
+console.log(word) 'dog'
+var word 'bird'
